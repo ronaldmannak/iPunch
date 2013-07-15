5fdc8d2b89088c59d723d06a85173e3cfcf76a66
Ronald Mannak <ronaldmannak@me.com>
2013-07-15 10:06:21 -0700

    Added punched card visualisation

--- iPunch/REMEditorViewController.m
+++ iPunch/REMEditorViewController.m
@@ -9,6 +9,8 @@
 #import "REMEditorViewController.h"
 #import "REMHollerithNumber.h"
 
+#define LINE_HEIGHT 17.0f
+
 @interface REMEditorViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *editor;
 @property (weak, nonatomic) IBOutlet UIView *cardView;
@@ -24,6 +26,12 @@
 	// Do any additional setup after loading the view.
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self updatePunchedCard];
+}
+
 - (void)updatePunchedCard
 {
     NSString *text = [self.editor.text uppercaseString];
@@ -36,19 +44,32 @@
     NSAssert([hollerithRepresentation count] == text.length, nil);
     
     // Add graphics
-    [self.cardView.subviews makeObjectsPerformSelector:@selector(removeSubView:)];
+    [self.cardView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    
+    __block CGFloat xPosition = 17.0f;
+    UIImage *punchImage = [UIImage imageNamed:@"Punch.png"];
     
     [hollerithRepresentation enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         NSArray *punchesArray = ((REMHollerithNumber *) obj).arrayValue;
         NSLog(@"H: %@", punchesArray);
-//        for (NSString *punch in punchesArray)
+        
+        CGFloat yPos = 3.0f + LINE_HEIGHT;
+        for (NSString *punch in punchesArray) {
+            if ([punch isEqualToString:@"X"]) {
+                UIImageView *imageView = [[UIImageView alloc] initWithImage:punchImage];
+                [self.cardView addSubview:imageView];
+                CGPoint origin = CGPointMake(xPosition, yPos);
+                CGRect frame = imageView.frame;
+                frame.origin = origin;
+                imageView.frame = frame;
+            }
+            yPos += LINE_HEIGHT;
+        }
+        xPosition += 6.0f;
+        // Print *, "Hello World!"
     }];
 }
 
-- (void)removeSubView: (UIView *)view
-{
-    [view removeFromSuperview];
-}
 - (void)textViewDidChange:(UITextView *)textView
 {
     [self updatePunchedCard];
